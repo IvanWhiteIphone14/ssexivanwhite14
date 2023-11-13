b0b25bc33a85146bde06ad3c333663387710a153
allen <allen@gmail.com>
2023-11-13 20:55:41 +0200

    Initial commit

---
+++ home/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from django.db import models
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    price = models.CharField(max_length=10)
+    short_info = models.TextField(default='short info')
+    fuel = models.TextField(default='N/A')
+    traction = models.TextField(default='N/A')
+    gearbox = models.TextField(default='Automatic')
+    engine = models.TextField(default='N/A')
+    year = models.IntegerField()
+    main_photo = models.ImageField(upload_to='taciki/', default='default_photo.jpg')
+    type = models.CharField(max_length=100, default='Coupe')
+
+    def __str__(self):
+        return f"{self.brand} {self.model} {self.type}({self.year}) {self.engine}"
+
+
+
+class CarPhoto(models.Model):
+    car = models.ForeignKey(Car, on_delete=models.CASCADE)
+    photo = models.ImageField(upload_to='car_photos/')
+
+# Create your models here.
